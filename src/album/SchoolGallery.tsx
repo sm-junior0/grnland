9504dc580e84803df857c88f873e1d9bc1a2908e
Manene <juniormanene@gmail.com>
2025-07-07 19:36:58 +0200

    gallery

--- src/album/SchoolGallery.tsx
+++ src/album/SchoolGallery.tsx
@@ -79,6 +79,7 @@ const SchoolGallery = () => {
           </div>
         </div>
       </div>
+      
       <div className="pt-20 pb-8 px-4 sm:px-6 lg:px-8">
         <div className="max-w-7xl mx-auto">
           <div className={`text-center mb-12 scroll-fade-in ${galleryAnimation.isVisible ? 'visible' : ''}`} ref={galleryAnimation.ref}>
